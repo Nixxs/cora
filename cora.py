287d83a9de8e29efbebb23b0dc9fd69e42b9de52
Nicholas Chai <nicholas.chai@outlook.com>
2023-10-31 23:17:59 +0800

    added a take command function, when executed it will listen for audio send it to google to convert to text and return the value from there we feed that text input into the speak function to pump it out the speakers

--- cora.py
+++ cora.py
@@ -1,5 +1,6 @@
 import pyaudio
 import pyttsx3
+import speech_recognition as sr
 
 salliVoiceIndex = 1
 
@@ -7,7 +8,29 @@ def speak(text):
     engine = pyttsx3.init("sapi5")
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[salliVoiceIndex].id)
+    # this just slows the voice down a little
+    engine.setProperty('rate', 175) 
     print(f"CORA: {text}\n")
     engine.say(text)
     engine.runAndWait()
 
+def takeCommand():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening..", end="")
+        audio = recognizer.listen(source)
+        query = ""
+
+        try:
+            print("Recognizing..", end="")
+            query = recognizer.recognize_google(audio, language="en-AU")
+            print(f"User said: {query}")
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+    return query.lower()
+
+def main():
+    said = takeCommand()
+    speak(f"I heard you say, {said}")
+
+main()
