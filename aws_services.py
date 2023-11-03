1ff4f204017f94f0490348c7301af8a81e0b6c6c
Nicholas Chai <nicholas.chai@outlook.com>
2023-11-03 11:02:07 +0800

    fixed an issue with the get_polly_client function not actualyl being a function

--- aws_services.py
+++ aws_services.py
@@ -2,9 +2,11 @@ import boto3
 import config
 
 # Initialize a session using Amazon Polly
-polly_client = boto3.client(
-    'polly',
-    aws_access_key_id=config.AWS_ACCESS_KEY,
-    aws_secret_access_key=config.AWS_SECRET_KEY,
-    region_name=config.AWS_REGION
-)
+def get_polly_client():
+    polly_client = boto3.client(
+        'polly',
+        aws_access_key_id=config.AWS_ACCESS_KEY,
+        aws_secret_access_key=config.AWS_SECRET_KEY,
+        region_name=config.AWS_REGION
+    )
+    return polly_client
