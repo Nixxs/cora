ac952094b5c1896371288f8c8c121e1ed4da9d48
Nicholas Chai <nicholas.chai@outlook.com>
2023-11-04 07:23:16 +0800

    changed the model to the latest version of 3.5 this should be faster than 4 but also includes function calling

--- openai_services.py
+++ openai_services.py
@@ -2,6 +2,7 @@ import openai
 import config
 
 openai.api_key = config.OPENAI_KEY
+chatGPTModel = "gpt-3.5-turbo-0613"
 
 preprompt = "you are helping my personal voice assistant produce meaningful but concise responses to my voice prompts."
 conversation_history = []
@@ -16,27 +17,18 @@ def get_current_models():
 
 def get_chatgpt_response(prompt):
     global conversation_history
-    # if this is the first prompt then  create the initial messages otherwise just append the current message
+    # if this is the first prompt then setup the conversation and intialise conversation_history
     if len(conversation_history) == 0:
         conversation_history = [
-            {
-                "role": "system",
-                "content": preprompt
-            },
-            {
-                "role": "user",
-                "content": prompt
-            }
+            {"role": "system","content": preprompt},
+            {"role": "user","content": prompt}
         ]
     else:
         conversation_history.append(
-            {
-                "role": "user",
-                "content": prompt
-            }
+            {"role": "user","content": prompt}
         )
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model=chatGPTModel,
         temperature=0,
         messages=conversation_history
     )
