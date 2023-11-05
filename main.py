491405cc3ef97c14514acd693b17388341c5e5c9
Nicholas Chai <nicholas.chai@outlook.com>
2023-11-05 21:47:41 +0800

    split up execution of voice recognition into 2 loops to handle wake words initialise the conversation with the wake word then continue conversation until you tell cora to sleep or shutdown need to change the way wakewords are detected at some point so that we aren't sending evetryting to google all the time.

--- main.py
+++ main.py
@@ -2,26 +2,72 @@ import time
 from audio_util import speak, listen
 from openai_services import get_chatgpt_response
 
-def greeting():
-    speak("Hi, how can I help you today?")
+cora_is_running = True
+wake_words = ["cora", "kora", "quora", "korra", "kooora"]
+user = "Nick"
 
-def main():
-    talking = True
-    greeting()
-    while talking:
-        userSaid = listen()
+def user_said_shutdown(user_said):
+    user_said = user_said.lower()
+    if "shutdown" in user_said or "shut down" in user_said:
+        return True
+    else:
+        return False
 
-        if "bye" in userSaid:
-            talking = False
-            speak("okay, see you next time.")
-        else:
-            # don't make any calls to chatgpt if a falst postiive is given
-            if not(userSaid == ""):
-                chatgpt_response = get_chatgpt_response(userSaid)
+def user_said_sleep(user_said):
+    user_said = user_said.lower()
+    if "sleep" in user_said:
+        return True
+    else:
+        return False
+
+# the main conversation loop after wake-up word was detected
+def run_conversation(initial_query):
+    global cora_is_running
+    initialized = False
+    while True:
+        # if we've already handled the initial query then continue the conversation and listen for the next prompt otherwise handle the initial query
+        if initialized:
+            user_query = listen().lower()
+
+            if user_said_sleep(user_query):
+                # break out of the the loop go back to voice loop
+                break
+            if user_said_shutdown(user_query):
+                # break out of the loop and let voice shutdown
+                cora_is_running = False
+                break
+
+            if not(user_query == ""):
+                chatgpt_response = get_chatgpt_response(user_query)
                 speak(chatgpt_response)
+        else:
+            initialized = True
+
+            # if the user has woken up cora and asked to shutdown in the same sentance
+            if user_said_shutdown(initial_query):
+                # break out of the loop and let voice shutdown
+                cora_is_running = False
+                break
+        
+            chatgpt_response = get_chatgpt_response(initial_query)
+            speak(chatgpt_response)
 
         # have a small pause between listening loops
         time.sleep(1)
+
+def voice():
+    while cora_is_running:
+        user_said = listen().lower()
+
+        # look through the audio and if one of the wake-words have been detected start conversation
+        for wake_word in wake_words:
+            if wake_word in user_said:
+                print("wake word detected")
+                run_conversation(user_said)
+    print("shutting down.")
+
+def main():
+    voice()
 
 if __name__ == "__main__":
     main()
